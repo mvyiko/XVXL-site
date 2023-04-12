cb81817d934ab02e5c380a9563f0a4d108f5ce8c
Kwvus <iklvzss@gmail.com>
2023-04-12 14:02:47 +0200

    update

---
+++ adventurerankcalculator/script.js
@@ -0,0 +1,26 @@
+// Run functions when page loads
+intro();
+tagline();
+
+function intro() {
+let day = new Date();
+let hr = day.getHours();
+console.log(hr);
+
+if (hr >= 6 && hr <= 12) {
+  document.getElementById("header").innerHTML = "xvxl's EXP calculator<br>Good morning!"
+}
+if (hr >= 12 && hr <= 16) {
+  document.getElementById("header").innerHTML = "xvxl's EXP calculator<br>Enjoy your day!"
+}
+if (hr >= 16 && hr <= 23) {
+  document.getElementById("header").innerHTML = "xvxl's EXP calculator<br>Good evening!"
+}
+if (hr >= 23 || hr <= 6) {
+  document.getElementById("header").innerHTML = "xvxl's EXP calculator<br>Good night!"
+}
+}
+
+function tagline() {
+  document.getElementById("tagline").innerHTML = 'A <a class="textLink" id="githubLink" href="https://github.com/silentswrd">xvxl</a> site. ︱ <a class="textLink" id="githubLink" href="https://github.com/silentswrd">xvxl</a>l에서 만든 웹사이트'
+}
